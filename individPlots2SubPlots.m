e6c570860ed73f8760bd2c11c55832de5e7bd515
Duncan Tulimieri <tulimid@udel.edu>
2022-01-07 17:13:32 -0500

    updates

--- individPlots2SubPlots.m
+++ individPlots2SubPlots.m
@@ -12,12 +12,20 @@ given dimensions
 
 %}
 
+arguments 
+    axS (1,:) 
+    subplotDim (1, 2) 
+end
+if length(axS) > sum(subplotDim)
+    error('Sum of subplot dimension must be greater or equal to length of axis handles')
+end
+
 f = figure();
 axSP = gobjects(size(axS)); 
 for i = 1:sum(subplotDim)
     axSP(i) = subplot(subplotDim(1), subplotDim(2), i, 'parent', f);
 end
-for i = 1:sum(subplotDim)
+for i = 1:length(axS)
     axcp = copyobj(axS(i), f);
     set(axcp, 'position', get(axSP(i), 'position'));
     delete(axSP(i)); 
